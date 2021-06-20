4e2be62e0924ee3201cf4f28487621ffa9e395bb
ahmedeminboluk <ahmedeminboluk@gmail.com>
2021-06-20 19:37:48 +0300

    patika101

---
+++ Variables/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = string.Empty;
+            str1 = "Eren Yilmaz";
+            string ad = "Eren";
+            string soyad = "Yilmaz";
+            string tamIsım = ad + soyad;
+
+            int integer1 = 5;
+            int integer2 = 3;
+            int integer3 = integer1 + integer2;
+
+            bool bool1 = 10 > 2;
+
+
+            //Değişken Dönüşümleri
+            string str20 = "20";
+            int int20 = 20;
+
+            string yeniDeger = str20 + int20.ToString();
+            Console.WriteLine(yeniDeger); //2020
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21);
+
+            int int22 = int20 + int.Parse(str20);
+
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hour);
+        }
+    }
+}
---
+++ While/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("**** Foreach ******");
+            string[] arabalar = {"BMW", "Ford", "Toyota", "Nissan"};
+
+            foreach (string araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+
+        }
+    }
+}
